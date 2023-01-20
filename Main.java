70ac2bf77cf8eb352f5e0c6de6033ac151f731b4
Kashaeva <karrapiisboring@gmail.com>
2023-01-20 14:19:47 +0300

    попытка номер 1

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import java.util.*;
+
+public class Main  {
+    public static void main (String[] args) {
+        int[] firstArray = getRandomArray(12);
+        int[] secondArray = getRandomArray(12);
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < firstArray.length; i++) {
+            for (int j = 0; j < secondArray.length; j++) {
+                if (firstArray[i] == firstArray[j]) {
+                     int number = firstArray[i];
+                     set.add(number);
+                }
+
+            }
+        }
+
     }
 }
+
